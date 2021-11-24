e4adad3165ae35444814d074533cb50fcea5130e
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2021-11-24 13:51:41 +0700

    upload 24/11/2021

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,4 @@
+//Chuyển đổi giữa đăng kí đăng nhập
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
@@ -16,3 +17,25 @@ tabs.forEach((tab, index) => {
         console.log(this, pane)
     };
 });
+
+
+//Đóng mở user
+const openMenu = $('#showmenu')
+const hiddenMenu = $("#hidden")
+const sidemenu = $("#navbar-right")
+const overlay = $('#overlay')
+
+openMenu.addEventListener("click", function() {
+    sidemenu.classList.add("active");
+    overlay.classList.add("active");
+
+})
+
+hiddenMenu.addEventListener('click', function() {
+    sidemenu.classList.remove("active");
+    overlay.classList.remove("active");
+})
+
+overlay.addEventListener('click', function() {
+    overlay.closest('.overlay')
+})
